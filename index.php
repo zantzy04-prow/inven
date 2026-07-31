d7b9d2cea7420769dc6c57365ec05ba081cb1e1e
zantzy04-prow <zantzy04@gmail.com>
2026-07-31 21:25:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
+
 require_once __DIR__ . '/config/db.php';
 require_once __DIR__ . '/includes/auth.php';
 
